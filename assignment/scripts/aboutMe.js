296a062a1f24ab614d06b2a5ef44d7a0b5cad566
Ilyas Mohamed <zswaleh@ymail.com>
2023-03-04 21:43:33 -0600

    done with assignment

--- assignment/scripts/aboutMe.js
+++ assignment/scripts/aboutMe.js
@@ -60,7 +60,13 @@ else if(pets > allowedPets){
 // assigns the highest value to `mostPets`. There's several possibilities --
 // be sure to think through all the scenarios. 
 // console.log `mostPets` after the conditional has run.
-let mostPets 
+let mostPets;
+if (pets > friendsPets) {
+  mostPets = pets
+} else if (pets < friendsPets) {
+  mostPets = friendsPets
+}
+console.log(mostPets);
 // 17 - Write a *switch* statement that logs:
 //      "First is the worst" if your lucky number is 1
 //      "Second is the best" if your lucky number is 2
